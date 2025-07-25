dd725374d4caa3c5a51f39d2c357249abe893dff
tyrannosaur56-sys <tyrannosaur56@gmail.com>
2025-07-26 02:56:28 +0800

    Update server.py

--- server.py
+++ server.py
@@ -77,7 +77,7 @@ def webhook():
         return jsonify({"error": str(e)}), 500
 
     if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 4242)))
+        app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 4242)))
 
 
 
